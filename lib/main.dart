1480cd7d17d28ffea5e6e49103b58a1e0e3dfa81
cerberusprogrammer <cerberusprogrammer@gmail.com>
2024-09-03 14:35:42 +0000

    Functional canvas (no drawing already)

--- lib/main.dart
+++ lib/main.dart
@@ -14,26 +14,78 @@ class MainPage extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Clean Drawer')),
-        body: GestureDetector(
+          appBar: AppBar(title: const Text('Clean Drawer')),
+          body: const CanvasView()),
+    );
+  }
+}
+
+class CanvasView extends StatefulWidget {
+  const CanvasView({super.key});
+
+  @override
+  State<CanvasView> createState() => _CanvasViewState();
+}
+
+class _CanvasViewState extends State<CanvasView> {
+  final ScrollController _horizontalController = ScrollController();
+  final ScrollController _verticalController = ScrollController();
+  double _currentX = 0;
+  double _currentY = 0;
+  bool drawMode = false;
+
+  List<Offset> points = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _horizontalController.addListener(_updatePosition);
+    _verticalController.addListener(_updatePosition);
+  }
+
+  void _updatePosition() => setState(() {
+        _currentX = _horizontalController.offset;
+        _currentY = _verticalController.offset;
+      });
+
+  void _startDrawing(Offset position) => setState(() => points = [position]);
+
+  void _updateDrawing(Offset position) => setState(() => points.add(position));
+
+  @override
+  void dispose() {
+    _horizontalController.removeListener(_updatePosition);
+    _verticalController.removeListener(_updatePosition);
+    _horizontalController.dispose();
+    _verticalController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        onPanUpdate: (details) {
+          _horizontalController
+              .jumpTo(_horizontalController.offset - details.delta.dx);
+          _verticalController
+              .jumpTo(_verticalController.offset - details.delta.dy);
+        },
+        child: SingleChildScrollView(
+          controller: _horizontalController,
+          scrollDirection: Axis.horizontal,
           child: SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: SizedBox(
-                width: 10000,
-                height: 10000,
-                child: Stack(
-                  children: [
-                    CustomPaint(
-                        painter: LinePainter(const [
-                      Offset(100, 100),
-                      Offset(200, 200),
-                      Offset(300, 300),
-                      Offset(400, 400),
-                    ]))
-                  ],
-                ),
+            controller: _verticalController,
+            scrollDirection: Axis.vertical,
+            child: SizedBox(
+              width: 10000,
+              height: 10000,
+              child: Stack(
+                children: [
+                  CustomPaint(
+                    painter: LinePainter(points),
+                  ),
+                ],
               ),
             ),
           ),
